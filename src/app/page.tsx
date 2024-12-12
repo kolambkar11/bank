0035dfdb16f6e5960b2bb86cea4e709631e3c97d
kolambkar11 <kolambkarashutosh@gmail.com>
2024-12-12 18:31:39 +0530

    form development in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, BadgeInfo } from "lucide-react";
 import {
   Accordion,
   AccordionContent,
@@ -159,16 +159,22 @@ export default function Home() {
                 <h1 className="text-xl font-bold my-4">Other Details</h1>
                 <div className="flex justify-between items-center gap-x-4">
                   <div className="w-full">
-                    <span className="text-xs text-gray-500">Name</span>
-                    <Input type="text" placeholder="Either or Survior" />
+                    <span className="text-xs text-gray-500">
+                      Passport Number
+                    </span>
+                    <Input type="text" placeholder="Passport Number" />
                   </div>
                   <div className="w-full">
-                    <span className="text-xs text-gray-500">Customer</span>
-                    <Input type="date" placeholder="Either or Survior" />
+                    <span className="text-xs text-gray-500">
+                      Date of Passport Issue
+                    </span>
+                    <Input type="date" placeholder="Date of Passport Issue" />
                   </div>
                   <div className="w-full">
-                    <span className="text-xs text-gray-500">PAN</span>
-                    <Input type="text" placeholder="Either or Survior" />
+                    <span className="text-xs text-gray-500">
+                      Place of Passport issue
+                    </span>
+                    <Input type="text" placeholder="Place of Passport issue" />
                   </div>
                 </div>
                 <div className="flex justify-between items-center gap-x-4 mt-4">
@@ -213,6 +219,192 @@ export default function Home() {
                   </div>
                 </div>
                 <div className="w-full">Rupees 10Lakhs Only</div>
+
+                <div className="flex items-center alert mt-4 p-3 bg-blue-50 rounded-xl font-bold text-blue-800">
+                  <BadgeInfo className="mr-3" /> Please make sure amount entered
+                  in NR is matching with the amount mentioned in 15CA/CB
+                  certification.
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+
+        <div className="accordion mt-6">
+          <Accordion type="single" defaultValue="item-1" className="w-full">
+            <AccordionItem value="item-1">
+              <AccordionTrigger className="bg-blue-50 px-4 decoration-muted font-bold rounded-tr-2xl rounded-tl-2xl">
+                Credit Account Details (Overseas Account)
+              </AccordionTrigger>
+              <AccordionContent className="p-4 border-r-2 rounded-br-2xl border-l-2 rounded-bl-2xl">
+                <h1 className="font-bold mb-4">Beneficiary/Receiver Details</h1>
+                <div className="flex items-center gap-x-4">
+                  <div className="w-1/4">
+                    <span className="text-xs text-gray-500">
+                      Name of the Receiver
+                    </span>
+                    <Input type="text" placeholder="Name of the Receiver" />
+                  </div>
+                  <div className="w-1/4">
+                    <span className="text-xs text-gray-500">
+                      Credit Account Number
+                    </span>
+                    <Input type="text" placeholder="Credit Account Number" />
+                  </div>
+                </div>
+                <h1 className="font-bold my-4">Address of the Receiver</h1>
+                <div className="grid grid-cols-3 gap-x-7 gap-y-4 ">
+                  <div className="w-full">
+                    <span className="text-xs text-gray-500">
+                      Address Line 1
+                    </span>
+                    <Input type="text" placeholder="Address Line 1" />
+                  </div>
+                  <div className="w-full">
+                    <span className="text-xs text-gray-500">
+                      Address Line 2
+                    </span>
+                    <Input type="text" placeholder="Address Line 2" />
+                  </div>
+                  <div className="w-full">
+                    <span className="text-xs text-gray-500">
+                      Address Line 3
+                    </span>
+                    <Input type="text" placeholder="Address Line 3" />
+                  </div>
+                  <div className="w-full">
+                    <span className="text-xs text-gray-500">PIN</span>
+                    <Input type="text" placeholder="PIN" />
+                  </div>
+                  <div className="w-full">
+                    <span className="text-xs text-gray-500">City</span>
+                    <Input type="text" placeholder="City" />
+                  </div>
+                  <div className="w-full">
+                    <span className="text-xs text-gray-500">State</span>
+                    <Input type="text" placeholder="State" />
+                  </div>
+                  <div className="w-full">
+                    <span className="text-xs text-gray-500">Country</span>
+                    <Input type="text" placeholder="Country" />
+                  </div>
+                </div>
+                <div className="">
+                  <h1 className="font-bold my-4">Beneficiary Bank Details</h1>
+                  <div className="grid grid-cols-3 gap-3">
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">
+                        Name of the Bank
+                      </span>
+                      <Input type="text" placeholder="Name of the Bank" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">Swift Code</span>
+                      <Input type="text" placeholder="Swift Code" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">IBAN</span>
+                      <Input type="text" placeholder="IBAN" />
+                    </div>
+                  </div>
+                </div>
+                <div className="">
+                  <h1 className="font-bold my-4">Beneficiary Bank Address</h1>
+                  <div className="grid grid-cols-3 gap-3">
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">
+                        Address Line 1
+                      </span>
+                      <Input type="text" placeholder="Address Line 1" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">
+                        Address Line 2
+                      </span>
+                      <Input type="text" placeholder="Address Line 2" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">
+                        Address Line 3
+                      </span>
+                      <Input type="text" placeholder="Address Line 3" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">PIN</span>
+                      <Input type="text" placeholder="PIN" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">City</span>
+                      <Input type="text" placeholder="City" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">State</span>
+                      <Input type="text" placeholder="State" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">Country</span>
+                      <Input type="text" placeholder="Country" />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="">
+                  <h1 className="font-bold my-4">
+                    Correspondence Bank Details
+                  </h1>
+                  <div className="grid grid-cols-3 gap-3">
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">
+                        Name of the Bank
+                      </span>
+                      <Input type="text" placeholder="Name of the Bank" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">Swift Code</span>
+                      <Input type="text" placeholder="Swift Code" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">IBAN</span>
+                      <Input type="text" placeholder="IBAN" />
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-3 gap-3">
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">
+                        Address Line 1
+                      </span>
+                      <Input type="text" placeholder="Address Line 1" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">
+                        Address Line 2
+                      </span>
+                      <Input type="text" placeholder="Address Line 2" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">
+                        Address Line 3
+                      </span>
+                      <Input type="text" placeholder="Address Line 3" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">PIN</span>
+                      <Input type="text" placeholder="PIN" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">City</span>
+                      <Input type="text" placeholder="City" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">State</span>
+                      <Input type="text" placeholder="State" />
+                    </div>
+                    <div className="w-full">
+                      <span className="text-xs text-gray-500">Country</span>
+                      <Input type="text" placeholder="Country" />
+                    </div>
+                  </div>
+                </div>
               </AccordionContent>
             </AccordionItem>
           </Accordion>
